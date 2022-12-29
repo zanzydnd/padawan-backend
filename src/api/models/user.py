d6cc6d9626d9947d800ee44e08d848dbc8e77aea
Даниил Козлов <zanzydnd@gmail.com>
2022-12-29 20:35:06 +0300

    user model + some trash

---
+++ src/api/models/user.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class PadawanUser(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email", "password"]
+
+    class UserStatus(models.TextChoices):
+        TEACHER = "Учитель"
+        PADAWAN = "Ученик"
+
+    username = None  # отключаем username пользователю.
+    status = models.CharField(255, choices=UserStatus.choices, default=UserStatus.PADAWAN)
+
+    def is_teacher(self):
+        return self.status == self.UserStatus.TEACHER
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        db_table = "padawan_user"
+
+
+class ContactService(models.Model):
+    name = models.CharField(255, verbose_name="Название", unique=True)
+    image = models.ImageField(upload_to="contact_services", verbose_name="Картинка для фронт-енда")
+
+    class Meta:
+        verbose_name = "Сервис связи"
+        verbose_name_plural = "Сервисы связи"
+        db_table = "contact_services"
+
+
+class PadawanUserContactChoices(models.Model):
+    class ChoiceType(models.TextChoices):
+        PRIMARY = "Первостепенный"
+        SECONDARY = "Второстепенный"
+
+    user = models.ForeignKey(PadawanUser, on_delete=models.CASCADE, related_name="possible_contacts")
+    contact_service = models.ForeignKey(ContactService, on_delete=models.CASCADE, related_name="users_choice")
+
+    type = models.CharField(255, choices=ChoiceType.choices, default=ChoiceType.SECONDARY, verbose_name="Тип")
+    value = models.CharField(255, verbose_name="Значение")
+
+    class Meta:
+        verbose_name = "Контакт пользователя"
+        verbose_name_plural = "Контакты пользователей"
+        db_table = "contact_services"
